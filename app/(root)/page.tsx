96d5439567c7cc1fcff26e997a7f2eb995c699cf
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-13 12:49:04 +0545

    Implement real-time search fetching with LiveAPI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,16 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   console.log(JSON.stringify(posts, null, 2));
 
@@ -44,6 +45,8 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
