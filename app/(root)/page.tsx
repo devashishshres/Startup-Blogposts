31fe82e8d433a91e6b8913a9df2788165d90634b
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-21 00:09:17 +0545

    Integrate auth session in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 
 export default async function Home({ searchParams }: {
@@ -10,9 +11,11 @@ export default async function Home({ searchParams }: {
   const query = (await searchParams).query;
   const params = { search: query || null };
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const session = await auth();
 
-  console.log(JSON.stringify(posts, null, 2));
+  console.log(session?.id);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
