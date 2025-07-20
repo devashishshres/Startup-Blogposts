85cf9fe8de6f744a9067cb18c3911bc0740891b0
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-21 00:09:31 +0545

    Add author queries for GitHub authentication

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -37,3 +37,27 @@ export const STARTUP_VIEWS_QUERY =
     _id, views
     }
 `);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+    *[_type == "author" && _id == $id][0]{
+        _id,
+        id,
+        name,
+        username,
+        email,
+        image,
+        bio
+    }
+`);
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(`
+*[_type == "author" && _id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+}
+`);
