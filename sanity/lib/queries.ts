eb9cab3c8256656c7efe38ac6a7d457a4ef89fa1
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-31 10:19:52 +0545

    Add STARTUPS_BY_AUTHOR_QUERY query to get startups created by a user.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,6 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, 
   title, 
   slug,
@@ -15,8 +14,7 @@ export const STARTUPS_QUERY =
   image,
 }`);
 
-export const STARTUP_BY_ID_QUERY =
-    defineQuery(`*[_type == "startup" && _id == $id][0]{
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id, 
   title, 
   slug,
@@ -60,3 +58,17 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
     bio
 }
 `);
+
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
